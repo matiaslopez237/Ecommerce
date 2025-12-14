baa0382b9a2c54689b1eddefcca3ef9773ababcc
Matu <matias.e.lopez747@gmail.com>
2025-12-14 20:59:08 -0300

    intento 5

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("API funcionando");
 });
 
