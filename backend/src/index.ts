17c0e123b1f5f2f93f104f31a2961627ca3cf19f
Matu <matias.e.lopez747@gmail.com>
2025-12-14 19:02:54 -0300

    creo carpeta de backend

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API funcionando");
+});
+
+app.listen(3000, () => console.log("API escuchando en http://localhost:3000"));
