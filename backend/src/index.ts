566adc7fcbddfaadd2d29b18a8ba1f7bab0df6f3
Matu <matias.e.lopez747@gmail.com>
2025-12-19 23:17:10 -0300

    agrego orders

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import authRoutes from "./routes/auth.js";
 import productsRoutes from "./routes/products.js";
 import categoriesRoutes from "./routes/categories.js";
 import cartRoutes from "./routes/cart.js";
+import ordersRoutes from "./routes/orders.js";
 
 const app = express();
 app.use(express.json());
@@ -22,6 +23,8 @@ app.use("/products", productsRoutes);
 app.use("/categories", categoriesRoutes);
 
 app.use("/cart", cartRoutes);
+
+app.use("/orders", ordersRoutes);
 
 app.listen(3000, () => {
   console.log("API escuchando en http://localhost:3000");
